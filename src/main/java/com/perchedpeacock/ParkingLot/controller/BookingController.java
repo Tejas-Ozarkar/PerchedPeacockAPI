8cd93fa95e346ab980c88c9a427ae79eba7d5c94
Tejas-Ozarkar <tejozarkar@gmail.com>
2019-10-14 08:16:11 +0530

    FEATURE: Booking Service & Controller

--- src/main/java/com/perchedpeacock/ParkingLot/controller/BookingController.java
+++ src/main/java/com/perchedpeacock/ParkingLot/controller/BookingController.java
@@ -1,12 +1,13 @@
 package com.perchedpeacock.ParkingLot.controller;
 
 import com.perchedpeacock.ParkingLot.model.Booking;
+import com.perchedpeacock.ParkingLot.model.HttpResponse;
 import com.perchedpeacock.ParkingLot.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -16,10 +17,18 @@ public class BookingController {
     @Autowired
     BookingService bookingService;
 
+    @GetMapping()
+    public ResponseEntity<List<Booking>> getBookings(){
+        List<Booking> bookings = bookingService.getBookings();
+        return ResponseEntity.status(200).body(bookings);x
+    }
+
     @PostMapping("/create")
-    public String createBooking(@RequestBody Booking booking){
+    public ResponseEntity<HttpResponse> createBooking(@RequestBody Booking booking){
         bookingService.createBooking(booking);
-        return "Booking created";
+        HttpResponse response = new HttpResponse();
+        response.setMessage("Booking created");
+        return ResponseEntity.status(200).body(response);
     }
 
     @PostMapping("/checkout")
--- src/main/java/com/perchedpeacock/ParkingLot/service/BookingService.java
+++ src/main/java/com/perchedpeacock/ParkingLot/service/BookingService.java
@@ -1,17 +1,18 @@
 package com.perchedpeacock.ParkingLot.service;
 
-import com.perchedpeacock.ParkingLot.model.Booking;
-import com.perchedpeacock.ParkingLot.model.ParkingLot;
-import com.perchedpeacock.ParkingLot.model.ParkingSpace;
+import com.perchedpeacock.ParkingLot.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
 
+import java.awt.print.Book;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class BookingService {
@@ -19,7 +20,25 @@ public class BookingService {
     @Autowired
     MongoOperations mongoOperations;
 
+    @Autowired
+    UserService userService;
+
+    public List<Booking> getBookings(){
+        User user = userService.getAuthorizeUser();
+        Query query1 = new Query(Criteria.where("userId").is(user.getId()));
+        List<Vehicle> vehicles = mongoOperations.find(query1, Vehicle.class);
+        List<Booking> bookings = new ArrayList<Booking>();
+        for(int i = 0;i<vehicles.size(); i++){
+            Query query2 = new Query(Criteria.where("vehicleId").is(vehicles.get(i).getId()));
+            bookings.addAll(mongoOperations.find(query2, Booking.class));
+        }
+        return bookings;
+    }
+
     public void createBooking(Booking booking) {
+        ParkingLot parkingLot = mongoOperations.findById(booking.getLotId(), ParkingLot.class);
+        parkingLot.setStatus(false);
+        mongoOperations.save(parkingLot);
         mongoOperations.save(booking);
     }
 
@@ -39,6 +58,9 @@ public class BookingService {
             System.out.println("Failed to date");
         }
         mongoOperations.save(booking);
+        ParkingLot parkingLot = mongoOperations.findById(booking.getLotId(), ParkingLot.class);
+        parkingLot.setStatus(true);
+        mongoOperations.save(parkingLot);
         System.out.println("checkout");
     }
 
